eb17d59b5002923437525230539558dd8eb90b8f
leobewater <mmbee888@gmail.com>
2024-06-30 07:16:32 -0400

    modify questions in admin

--- pollproject/pollApp/admin.py
+++ pollproject/pollApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+admin.site.site_header = "The Poll Mall"
+admin.site.site_title = "Voting Admin Area"
+admin.site.index_title = "Welcome to our Voting Admin Area"
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {
+        'fields': ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
